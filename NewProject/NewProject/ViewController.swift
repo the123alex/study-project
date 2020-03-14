88c097dd06766715a39d590cb88f482757f4d049
Aleksey <alex00836@yandex.ru>
2020-03-14 19:13:09 +0300

    Create project

---
+++ NewProject/NewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Aleksey on 14.03.2020.
+//  Copyright © 2020 Aleksey Mikhlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
