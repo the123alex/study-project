e946885a61ca57e357f828fa8f891b958203c5ca
Aleksey <alex00836@yandex.ru>
2020-03-21 11:21:24 +0300

    Fix SwiftLint

--- NewProject/NewProject/ViewController.swift
+++ NewProject/NewProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstView: UIView!
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var thirdView: UIView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,11 +21,25 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.firstView.backgroundColor = UIColor(red: CGFloat.random(in: 0.01...1.0), green: CGFloat.random(in: 0.01...1.0), blue: CGFloat.random(in: 0.01...1.0), alpha: 1.0)
-            self.secondView.backgroundColor = UIColor(red: CGFloat.random(in: 0.01...1.0), green: CGFloat.random(in: 0.01...1.0), blue: CGFloat.random(in: 0.01...1.0), alpha: 1.0)
-            self.thirdView.backgroundColor = UIColor(red: CGFloat.random(in: 0.01...1.0), green: CGFloat.random(in: 0.01...1.0), blue: CGFloat.random(in: 0.01...1.0), alpha: 1.0)
+            self.firstView.backgroundColor = UIColor(
+                red: CGFloat.random(in: 0.01...1.0),
+                green: CGFloat.random(in: 0.01...1.0),
+                blue: CGFloat.random(in: 0.01...1.0),
+                alpha: 1.0
+            )
+            self.secondView.backgroundColor = UIColor(
+                red: CGFloat.random(in: 0.01...1.0),
+                green: CGFloat.random(in: 0.01...1.0),
+                blue: CGFloat.random(in: 0.01...1.0),
+                alpha: 1.0
+            )
+            self.thirdView.backgroundColor = UIColor(
+                red: CGFloat.random(in: 0.01...1.0),
+                green: CGFloat.random(in: 0.01...1.0),
+                blue: CGFloat.random(in: 0.01...1.0),
+                alpha: 1.0
+            )
         }
     }
 }
