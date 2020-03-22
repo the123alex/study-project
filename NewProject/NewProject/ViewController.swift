002df4524f86946f9cd58a3e5b8e44733977912f
Aleksey <alex00836@yandex.ru>
2020-03-22 15:41:40 +0300

    Add background

--- NewProject/NewProject/ViewController.swift
+++ NewProject/NewProject/ViewController.swift
@@ -10,36 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var firstView: UIView!
-    @IBOutlet weak var secondView: UIView!
-    @IBOutlet weak var thirdView: UIView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.firstView.backgroundColor = UIColor(
-                red: CGFloat.random(in: 0.01...1.0),
-                green: CGFloat.random(in: 0.01...1.0),
-                blue: CGFloat.random(in: 0.01...1.0),
-                alpha: 1.0
-            )
-            self.secondView.backgroundColor = UIColor(
-                red: CGFloat.random(in: 0.01...1.0),
-                green: CGFloat.random(in: 0.01...1.0),
-                blue: CGFloat.random(in: 0.01...1.0),
-                alpha: 1.0
-            )
-            self.thirdView.backgroundColor = UIColor(
-                red: CGFloat.random(in: 0.01...1.0),
-                green: CGFloat.random(in: 0.01...1.0),
-                blue: CGFloat.random(in: 0.01...1.0),
-                alpha: 1.0
-            )
-        }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
     }
 }
