c302d1e98becd3b9a144cb47fc6be125806e7715
Aleksey <alex00836@yandex.ru>
2020-03-23 23:28:25 +0300

    Add: buttons, blur and label

--- NewProject/NewProject/ViewController.swift
+++ NewProject/NewProject/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var showWeather: UIButton!
+    @IBOutlet weak var aboutButton: UIButton!
+    @IBOutlet weak var blurView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showWeather.layer.cornerRadius = 20
+        aboutButton.layer.cornerRadius = 20
+
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = blurView.frame
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurView.addSubview(blurEffectView)
     }
 
     override func viewDidLayoutSubviews() {
