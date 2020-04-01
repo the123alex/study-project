26d9aff6e042d8d6cb4c071b64a05af8a60f79b2
Aleksey <alex00836@yandex.ru>
2020-04-01 15:46:49 +0300

    Add TabbleView and ViewCell - Before Homework

---
+++ NewProject/NewProject/Modules/WeatherList/Cells/WeatherListCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherListCell.swift
+//  NewProject
+//
+//  Created by Aleksey on 01.04.2020.
+//  Copyright © 2020 Aleksey Mikhlev. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListCell: UITableViewCell {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
--- NewProject/NewProject/Modules/WeatherList/WeatherListViewController.swift
+++ NewProject/NewProject/Modules/WeatherList/WeatherListViewController.swift
@@ -10,17 +10,54 @@
 import UIKit
 
 class WeatherListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     var presenter: WeatherListPresenter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
-
-       presenter.viewDidLoad()
+        setupViews()
+        presenter.viewDidLoad()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+    }
+
+    private func setupViews() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(
+            UINib(nibName: String(describing: WeatherListCell.self), bundle: Bundle.main),
+            forCellReuseIdentifier: String(describing: WeatherListCell.self)
+        )
+    }
+}
+
+extension WeatherListViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: WeatherListCell.self),
+            for: indexPath
+        ) as? WeatherListCell else {
+            fatalError("Wrong identifier")
+        }
+        cell.temperatureLabel.text = "11"
+      //  cell.temperatureLabel.text = String(Int.random(in: 1...100))
+        return cell
+    }
+}
+
+extension WeatherListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
