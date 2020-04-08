70cb892922520be5be48e0ec471a9b0347e152c6
Aleksey <alex00836@yandex.ru>
2020-04-08 12:02:43 +0300

    Add snapKIt

---
+++ NewProject/NewProject/Modules/WeatherList/Cells/WeatherListHeaderCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherListHeaderCell.swift
+//  NewProject
+//
+//  Created by Aleksey on 08.04.2020.
+//  Copyright © 2020 Aleksey Mikhlev. All rights reserved.
+//
+
+import UIKit
+
+let iconImageView = UIImageView()
+
+//let temperatureValueLabel: UILabel = {
+//    let label = UILabel()
+//    labe
+//}
